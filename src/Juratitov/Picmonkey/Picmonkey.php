ee8a40dc1373ea2a844318b00f9b657f3f9afd85
juratitov <juratitov@gmail.com>
2014-06-02 17:10:21 +0300

    Change config

--- src/Juratitov/Picmonkey/Picmonkey.php
+++ src/Juratitov/Picmonkey/Picmonkey.php
@@ -2,8 +2,6 @@
 
 namespace Juratitov\Picmonkey;
 
-use \Config;
-
 /**
  * Class Picmonkey
  * 
@@ -19,7 +17,7 @@ class Picmonkey
     public static function setConfig(array $config)
     {
         foreach ($config as $key => $value) {
-            Config::set('picmonkey::config.' . $key, $value);
+            \Config::set('picmonkey::config.' . $key, $value);
         }
     }
 
@@ -31,19 +29,19 @@ class Picmonkey
      */
     public static function getLoadingUrl($importUrl, $imageId)
     {
-        $query['_keyapi'] = Config::get('picmonkey::config._apikey');
+        $query['_keyapi'] = \Config::get('picmonkey::config._apikey');
         $query['_import'] = $importUrl;
         $query['_imageid'] = $imageId;
-        $query['_export'] = Config::get('picmonkey::config._export');
-        $query['_export_agent'] = Config::get('picmonkey::config._export_agent');
-        $query['_export_field'] = Config::get('picmonkey::config._export_field');
-        $query['_export_method'] = Config::get('picmonkey::config._export_method');
-        $query['_replace'] = Config::get('picmonkey::config._replace');
-        $query['_out_format'] = Config::get('picmonkey::config._out_format');
-        $query['_close_target'] = Config::get('picmonkey::config._close_target');
-        $query['_redirect'] = Config::get('picmonkey::config._redirect');
+        $query['_export'] = \Config::get('picmonkey::config._export');
+        $query['_export_agent'] = \Config::get('picmonkey::config._export_agent');
+        $query['_export_field'] = \Config::get('picmonkey::config._export_field');
+        $query['_export_method'] = \Config::get('picmonkey::config._export_method');
+        $query['_replace'] = \Config::get('picmonkey::config._replace');
+        $query['_out_format'] = \Config::get('picmonkey::config._out_format');
+        $query['_close_target'] = \Config::get('picmonkey::config._close_target');
+        $query['_redirect'] = \Config::get('picmonkey::config._redirect');
 
-        return Config::get('picmonkey::config._base_url') . '?' . http_build_query($query);
+        return \Config::get('picmonkey::config._base_url') . '?' . http_build_query($query);
     }
 
 }
